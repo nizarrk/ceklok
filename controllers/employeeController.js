a612754c2869a588fb7ec949279e10422973b36d
Nizar <niezar.rk@gmail.com>
2020-01-02 20:05:26 +0700

    import csv done, tinggal benerin add nya

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -6,6 +6,172 @@ const bcrypt = require('bcrypt');
 const path = require('path');
 const fs = require('fs');
 const mkdirp = require('mkdirp');
+const csv = require('csvjson');
+
+const generateEmployeeCode = async (APP, req, index) => {
+  let tgl = new Date().getDate().toString();
+  if (tgl.length == 1) {
+    tgl = '0' + new Date().getDate().toString();
+  }
+  let month = (new Date().getMonth() + 1).toString();
+  if (month.length == 1) {
+    month = '0' + month;
+  }
+  let year = new Date()
+    .getFullYear()
+    .toString()
+    .slice(2, 4);
+  let time = year + month + tgl;
+  let pad = '0000';
+  let kode = '';
+  let add = 1;
+
+  if (index) {
+    add = index;
+  }
+
+  let res = await APP.models.company[req.user.db].mysql.employee.findAll({
+    limit: 1,
+    order: [['id', 'DESC']]
+  });
+
+  if (res.length == 0) {
+    console.log('kosong');
+    let str = '' + 1;
+
+    kode = req.user.code + '-' + time + '-' + str;
+
+    return kode;
+  } else {
+    console.log('ada');
+    let lastID = res[0].employee_code;
+    let replace = lastID.replace(req.user.code + '-', '');
+    let lastNum = replace.charAt(replace.length - 1);
+
+    let num = parseInt(lastNum) + add;
+
+    kode = req.user.code + '-' + time + num;
+
+    return kode;
+  }
+};
+
+const checkEmployeeEntry = (APP, req, callback) => {
+  async.waterfall(
+    [
+      // function checkUsernameEmployee(callback) {
+      //   APP.models.company[req.user.db].mysql.employee
+      //     .findAll({
+      //       where: {
+      //         company_code: req.body.company,
+      //         user_name: req.body.username
+      //       }
+      //     })
+      //     .then(res => {
+      //       if (res && res.length > 0) {
+      //         callback({
+      //           code: 'DUPLICATE',
+      //           message: 'Error! Duplicate Username!',
+      //           info: {
+      //             dataCount: res.length,
+      //             parameter: 'username'
+      //           }
+      //         });
+      //       }
+      //       callback(null, {
+      //         code: 'NOT_FOUND',
+      //         info: {
+      //           parameter: 'username'
+      //         }
+      //       });
+      //     })
+      //     .catch(err => {
+      //       console.log('iki error username', err);
+
+      //       callback({
+      //         code: 'ERR_DATABASE',
+      //         data: JSON.stringify(err)
+      //       });
+      //     });
+      // },
+
+      function checkEmailEmployee(callback) {
+        APP.models.company[req.user.db].mysql.employee
+          .findAll({
+            where: {
+              email: req.body.email
+            }
+          })
+          .then(res => {
+            if (res && res.length > 0) {
+              callback({
+                code: 'DUPLICATE',
+                message: 'Error! Duplicate Email!',
+                info: {
+                  dataCount: res.length,
+                  parameter: 'email'
+                }
+              });
+            }
+            callback(null, {
+              code: 'NOT_FOUND',
+              info: {
+                parameter: 'email'
+              }
+            });
+          })
+          .catch(err => {
+            console.log('iki error email', err);
+
+            callback({
+              code: 'ERR_DATABASE',
+              data: JSON.stringify(err)
+            });
+          });
+      },
+
+      function checkTelpEmployee(result, callback) {
+        APP.models.company[req.user.db].mysql.employee
+          .findAll({
+            where: {
+              tlp: req.body.telp
+            }
+          })
+          .then(res => {
+            if (res && res.length > 0) {
+              callback({
+                code: 'DUPLICATE',
+                message: 'Error! Duplicate telp!',
+                info: {
+                  dataCount: res.length,
+                  parameter: 'telp'
+                }
+              });
+            }
+            callback(null, {
+              code: 'NOT_FOUND',
+              info: {
+                parameter: 'telp'
+              }
+            });
+          })
+          .catch(err => {
+            console.log('iki error telp', err);
+
+            callback({
+              code: 'ERR_DATABASE',
+              data: JSON.stringify(err)
+            });
+          });
+      }
+    ],
+    (err, result) => {
+      if (err) return callback(err);
+
+      callback(null, result);
+    }
+  );
+};
 
 exports.viewEmployeeInfo = (APP, req, callback) => {
   if (req.user.admin) {
@@ -43,171 +209,23 @@ exports.viewEmployeeInfo = (APP, req, callback) => {
 exports.addEmployee = (APP, req, callback) => {
   async.waterfall(
     [
-      function checkUsernameEmployee(callback) {
-        APP.models.company[req.user.db].mysql.employee
-          .findAll({
-            where: {
-              company_code: req.body.company,
-              user_name: req.body.username
-            }
-          })
-          .then(res => {
-            if (res && res.length > 0) {
-              callback({
-                code: 'DUPLICATE',
-                data: {
-                  row: 'Error! Duplicate Username!'
-                },
-                info: {
-                  dataCount: res.length,
-                  parameter: 'username'
-                }
-              });
-            }
-            callback(null, {
-              code: 'NOT_FOUND',
-              data: {
-                row: []
-              },
-              info: {
-                dataCount: res.length,
-                parameter: 'username'
-              }
-            });
-          })
-          .catch(err => {
-            console.log('iki error username', err);
-
-            callback({
-              code: 'ERR_DATABASE',
-              data: JSON.stringify(err)
-            });
-          });
+      function checkDataEntry(callback) {
+        checkEmployeeEntry(APP, req, callback);
       },
 
-      function checkEmailEmployee(result, callback) {
-        APP.models.company[req.user.db].mysql.employee
-          .findAll({
-            where: {
-              email: req.body.email
-            }
-          })
+      function generateCode(result, callback) {
+        let code = new Promise((resolve, reject) => {
+          resolve(generateEmployeeCode(APP, req));
+        });
+
+        code
           .then(res => {
-            if (res && res.length > 0) {
-              callback({
-                code: 'DUPLICATE',
-                data: {
-                  row: 'Error! Duplicate Email!'
-                },
-                info: {
-                  dataCount: res.length,
-                  parameter: 'email'
-                }
-              });
-            }
-            callback(null, {
-              code: 'NOT_FOUND',
-              data: {
-                row: []
-              },
-              info: {
-                dataCount: res.length,
-                parameter: 'email'
-              }
-            });
-          })
-          .catch(err => {
-            console.log('iki error email', err);
-
-            callback({
-              code: 'ERR_DATABASE',
-              data: JSON.stringify(err)
-            });
-          });
-      },
-
-      function checkTelpEmployee(result, callback) {
-        APP.models.company[req.user.db].mysql.employee
-          .findAll({
-            where: {
-              tlp: req.body.telp
-            }
-          })
-          .then(res => {
-            if (res && res.length > 0) {
-              callback({
-                code: 'DUPLICATE',
-                data: {
-                  row: 'Error! Duplicate telp!'
-                },
-                info: {
-                  dataCount: res.length,
-                  parameter: 'telp'
-                }
-              });
-            }
-            callback(null, {
-              code: 'NOT_FOUND',
-              data: {
-                row: []
-              },
-              info: {
-                dataCount: res.length,
-                parameter: 'telp'
-              }
-            });
-          })
-          .catch(err => {
-            console.log('iki error telp', err);
-
-            callback({
-              code: 'ERR_DATABASE',
-              data: JSON.stringify(err)
-            });
-          });
-      },
-
-      function generateEmployeeCode(result, callback) {
-        let tgl = new Date().getDate().toString();
-        let month = new Date().getMonth().toString();
-        let year = new Date()
-          .getFullYear()
-          .toString()
-          .slice(2, 4);
-        let time = year + month + tgl;
-        let pad = '0000';
-
-        APP.models.company[req.user.db].mysql.employee
-          .findAll({
-            limit: 1,
-            order: [['id', 'DESC']]
-          })
-          .then(res => {
-            if (res.length == 0) {
-              console.log('kosong');
-              let str = '' + 1;
-              let ans = pad.substring(0, pad.length - str.length) + str;
-
-              let kode = req.body.company + '-' + time + '-' + ans;
-
-              callback(null, kode);
-            } else {
-              console.log('ada');
-              let lastID = res[0].employee_code;
-              let replace = lastID.replace(req.body.company + '-' + time + '-', '');
-
-              let str = '' + parseInt(replace) + 1;
-              let ans = pad.substring(0, pad.length - str.length) + str;
-
-              let kode = req.body.company + '-' + time + '-' + ans;
-
-              callback(null, kode);
-            }
+            callback(null, res);
           })
           .catch(err => {
             console.log(err);
             callback({
-              code: 'ERR_DATABASE',
+              code: 'ERR',
               data: JSON.stringify(err)
             });
           });
@@ -267,7 +285,7 @@ exports.addEmployee = (APP, req, callback) => {
               msisdn: 'default',
               tlp: req.body.telp,
               email: req.body.email,
-              user_name: req.body.username,
+              user_name: data.kode.replace(req.user.code + '-', '') + '_' + req.body.name.split(' ')[0].toLowerCase(),
               password: data.encryptedPass,
               status: 1
             })
@@ -356,6 +374,203 @@ exports.addEmployee = (APP, req, callback) => {
             });
           })
           .catch(err => {
+            callback({
+              code: 'ERR_DATABASE',
+              data: JSON.stringify(err)
+            });
+          });
+      }
+    ],
+    (err, result) => {
+      if (err) return callback(err);
+
+      callback(null, result);
+    }
+  );
+};
+
+exports.importEmployeeData = (APP, req, callback) => {
+  async.waterfall(
+    [
+      function uploadDocuments(callback) {
+        trycatch(
+          () => {
+            if (!req.files || Object.keys(req.files).length === 0) {
+              return callback({
+                code: 'ERR',
+                message: 'No files were uploaded.'
+              });
+            }
+
+            let fileName = new Date().toISOString().replace(/:|\./g, '');
+            let importPath = `./public/uploads/company_${req.user.code}/employee/import/`;
+
+            if (!fs.existsSync(importPath)) {
+              mkdirp.sync(importPath);
+            }
+
+            if (req.files.import) {
+              req.files.import.mv(importPath + fileName + path.extname(req.files.import.name), function(err) {
+                if (err)
+                  return callback({
+                    code: 'ERR'
+                  });
+              });
+            }
+
+            callback(null, fileName + path.extname(req.files.import.name));
+          },
+          err => {
+            console.log(err);
+
+            callback({
+              code: 'ERR',
+              data: JSON.stringify(err)
+            });
+          }
+        );
+      },
+
+      function buildImported(result, callback) {
+        fs.readFile(`./public/uploads/company_${req.user.code}/employee/import/${result}`, 'utf8', (err, file) => {
+          if (err) {
+            callback({
+              code: 'ERR',
+              data: err
+            });
+          }
+          const dataObj = csv.toObject(file);
+          const arr = [];
+          dataObj.map((res, index) => {
+            let employeeCode = new Promise((resolve, reject) => {
+              resolve(generateEmployeeCode(APP, req, index + 1));
+            });
+
+            let newDate = new Date(res.dob);
+            let tgl = newDate.getDate() + 1;
+
+            if (tgl.length == 1) {
+              tgl = '0' + newDate.getDate().toString();
+            }
+
+            let month = newDate.getMonth() + 1;
+            let year = newDate.getFullYear().toString();
+
+            res.plainPassword = Math.random()
+              .toString(36)
+              .slice(-8);
+
+            res.status = 1;
+            res.dob = new Date(`${year}-${month}-${tgl}`);
+            res.company_code = req.user.code;
+            res.password = bcrypt.hashSync(res.plainPassword, 10);
+
+            employeeCode.then(code => {
+              res.employee_code = code;
+              res.user_name = code.replace(req.user.code + '-', '') + '_' + res.name.split(' ')[0].toLowerCase();
+              arr.push(res);
+
+              if (index + 1 == dataObj.length) {
+                callback(null, arr);
+              }
+            });
+          });
+        });
+      },
+
+      function checkEmailEmployee(result, callback) {
+        result.map((data, index) => {
+          APP.models.company[req.user.db].mysql.employee
+            .findAll({
+              where: {
+                email: data.email
+              }
+            })
+            .then(res => {
+              if (res && res.length > 0) {
+                return callback({
+                  code: 'DUPLICATE',
+                  message: `Error! Duplicate Email! ${data.email}`,
+                  info: {
+                    dataCount: res.length,
+                    parameter: 'email'
+                  }
+                });
+              }
+              if (result.length == index + 1) {
+                return callback(null, result);
+              }
+            })
+            .catch(err => {
+              console.log('iki error email', err);
+
+              return callback({
+                code: 'ERR_DATABASE',
+                data: JSON.stringify(err)
+              });
+            });
+        });
+      },
+
+      function checkTelpEmployee(result, callback) {
+        result.map((data, index) => {
+          APP.models.company[req.user.db].mysql.employee
+            .findAll({
+              where: {
+                tlp: data.tlp
+              }
+            })
+            .then(res => {
+              if (res && res.length > 0) {
+                return callback({
+                  code: 'DUPLICATE',
+                  message: `Error! Duplicate telp ${data.tlp}!`,
+                  info: {
+                    dataCount: res.length,
+                    parameter: 'telp'
+                  }
+                });
+              }
+              if (result.length == index + 1) {
+                return callback(null, result);
+              }
+            })
+            .catch(err => {
+              console.log('iki error telp', err);
+
+              return callback({
+                code: 'ERR_DATABASE',
+                data: JSON.stringify(err)
+              });
+            });
+        });
+      },
+
+      function insertData(result, callback) {
+        APP.models.company[req.user.db].mysql.employee
+          .bulkCreate(result)
+          .then(res => {
+            result.map(data => {
+              // send to email
+              APP.mailer.sendMail({
+                subject: 'Account Created',
+                to: data.email,
+                data: {
+                  username: data.user_name,
+                  pass: data.plainPassword
+                },
+                file: 'create_employee.html'
+              });
+            });
+
+            callback(null, {
+              code: 'INSERT_SUCCESS',
+              data: res
+            });
+          })
+          .catch(err => {
+            console.log('error insert', err);
+
             callback({
               code: 'ERR_DATABASE',
               data: JSON.stringify(err)
