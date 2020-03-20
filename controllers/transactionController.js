94cdfe20b278877ea517582781692c297110c754
Nizar <niezar.rk@gmail.com>
2020-03-20 18:15:27 +0700

    fix transaction + generate dayli absence need more fixing

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -2,184 +2,242 @@
 
 const async = require('async');
 
-exports.getByCompany = function(APP, req, callback) {
-  let mysql = APP.models.mysql;
-  let params = {
-    company_id: req.user.company
-  };
-
-  if (req.body.type) {
-    params.transaction_type_id = req.body.type;
-  }
-
-  if (req.body.status || req.body.status === 0) {
-    params.status = req.body.status;
-  }
-
-  if (req.body.datestart && req.body.dateend) {
-    params.date = {
-      $between: [req.body.datestart, req.body.dateend]
-    };
-  }
-
-  console.log(params);
-
-  console.log(mysql);
-
-  // add payment_method to payment
-  mysql.payment.belongsTo(mysql.payment_method, {
-    targetKey: 'id',
-    foreignKey: 'payment_method_id'
-  });
-
-  // add transaction_type to payment
-  mysql.payment.belongsTo(mysql.transaction_type, {
-    targetKey: 'id',
-    foreignKey: 'transaction_type_id'
-  });
-
-  // add payment_type to payment_method
-  mysql.payment_method.belongsTo(mysql.payment_type, {
-    targetKey: 'id',
-    foreignKey: 'payment_type_id'
-  });
-
-  mysql.payment
-    .findAll({
-      include: [
-        {
-          model: mysql.payment_method,
-          attributes: ['id', 'payment_type_id', 'to_bank_name', 'to_rek_name', 'to_rek_no'],
-          include: [
-            {
-              model: mysql.payment_type,
-              attributes: ['id', 'name']
-            }
-          ]
-        },
-        {
-          model: mysql.transaction_type,
-          attributes: ['id', 'name']
-        }
-      ],
-      where: params == {} ? 1 + 1 : params
-    })
-    .then(rows => {
-      return callback(null, {
-        code: rows && rows.length > 0 ? 'FOUND' : 'NOT_FOUND',
-        data: rows,
-        info: {
-          dataCount: rows.length
-        }
-      });
-    })
-    .catch(err => {
-      console.log(err);
-
-      return callback({
-        code: 'ERR_DATABASE',
-        data: err
-      });
-    });
-};
-
-exports.getAllCompany = function(APP, req, callback) {
-  let mysql = APP.models.mysql;
+exports.listAllTransaction = function(APP, req, callback) {
+  let { payment, payment_detail, payment_method, payment_type, transaction_type } = APP.models.mysql;
   let params = {};
 
-  if (req.body.company) {
-    params.company_id = req.body.company;
-  }
+  async.waterfall(
+    [
+      function checkLevel(callback) {
+        if (req.user.level === 1) {
+          callback(null, params);
+        } else if (req.user.level === 2) {
+          params = {
+            company_id: req.user.company
+          };
 
-  if (req.body.type) {
-    params.transcation_type_id = req.body.type;
-  }
-
-  if (req.body.status || req.body.status === 0) {
-    params.status = req.body.status;
-  }
-
-  if (req.body.datestart && req.body.dateend) {
-    params.date = {
-      $between: [req.body.datestart, req.body.dateend]
-    };
-  }
-
-  console.log(params);
-
-  // add company to payment
-  mysql.payment.belongsTo(mysql.company, {
-    targetKey: 'id',
-    foreignKey: 'company_id'
-  });
-
-  // add pricing to company
-  mysql.company.belongsTo(mysql.pricing, {
-    targetKey: 'id',
-    foreignKey: 'pricing_id'
-  });
-
-  // add payment_method to payment
-  mysql.payment.belongsTo(mysql.payment_method, {
-    targetKey: 'id',
-    foreignKey: 'payment_method_id'
-  });
-
-  // add transaction_type to payment
-  mysql.payment.belongsTo(mysql.transaction_type, {
-    targetKey: 'id',
-    foreignKey: 'transaction_type_id'
-  });
-
-  // add payment_type to payment_method
-  mysql.payment_method.belongsTo(mysql.payment_type, {
-    targetKey: 'id',
-    foreignKey: 'payment_type_id'
-  });
-
-  mysql.payment
-    .findAll({
-      include: [
-        {
-          model: mysql.company,
-          attributes: ['id', 'company_code', 'name', 'tlp', 'address'],
-          include: [
-            {
-              model: mysql.pricing,
-              attributes: ['id', 'name', 'description']
-            }
-          ]
-        },
-        {
-          model: mysql.payment_method,
-          attributes: ['id', 'payment_type_id', 'to_bank_name', 'to_rek_name', 'to_rek_no'],
-          include: [
-            {
-              model: mysql.payment_type,
-              attributes: ['id', 'name']
-            }
-          ]
-        },
-        {
-          model: mysql.transaction_type,
-          attributes: ['id', 'name']
+          callback(null, params);
+        } else {
+          callback({
+            code: 'INVALID_REQUEST',
+            id: '?',
+            message: 'Invalid User Level'
+          });
         }
-      ],
-      where: params == {} ? 1 + 1 : params
-    })
-    .then(rows => {
-      return callback(null, {
-        code: rows && rows.length > 0 ? 'FOUND' : 'NOT_FOUND',
-        data: rows,
-        info: {
-          dataCount: rows.length
+      },
+
+      function getData(params, callback) {
+        // if (req.body.type) {
+        //   params.transaction_type_id = req.body.type;
+        // }
+
+        if (req.body.status || req.body.status == '0') {
+          params.status = req.body.status;
         }
-      });
-    })
-    .catch(err => {
-      return callback({
-        code: 'ERR_DATABASE',
-        data: err
-      });
-    });
+
+        if (req.body.datestart && req.body.dateend) {
+          params.date = {
+            $between: [req.body.datestart, req.body.dateend]
+          };
+        }
+
+        console.log(params);
+
+        // add payment_method to payment
+        payment.belongsTo(payment_method, {
+          targetKey: 'id',
+          foreignKey: 'payment_method_id'
+        });
+
+        // add payment_type to payment_method
+        payment_method.belongsTo(payment_type, {
+          targetKey: 'id',
+          foreignKey: 'payment_type_id'
+        });
+
+        //add payment_detail to payment
+        payment.hasMany(payment_detail, {
+          sourceKey: 'id',
+          foreignKey: 'payment_id'
+        });
+
+        // add transaction_type to payment
+        payment_detail.belongsTo(transaction_type, {
+          targetKey: 'id',
+          foreignKey: 'transaction_type_id'
+        });
+
+        payment
+          .findAll({
+            include: [
+              {
+                model: payment_method,
+                attributes: ['id', 'payment_type_id', 'to_bank_name', 'to_rek_name', 'to_rek_no'],
+                include: [
+                  {
+                    model: payment_type,
+                    attributes: ['id', 'name']
+                  }
+                ]
+              },
+              {
+                model: payment_detail,
+                attributes: ['id', 'payment_id', 'transaction_type_id', 'item_id'],
+                include: [
+                  {
+                    model: transaction_type,
+                    attributes: ['id', 'name']
+                  }
+                ]
+              }
+            ],
+            where: params
+          })
+          .then(rows => {
+            if (rows.length == 0) {
+              callback({
+                code: 'NOT_FOUND',
+                id: '?',
+                message: 'Data tidak ditemukan'
+              });
+            } else {
+              callback(null, {
+                code: 'FOUND',
+                id: '?',
+                message: 'Data ditemukan',
+                data: rows
+              });
+            }
+          })
+          .catch(err => {
+            console.log(err);
+
+            return callback({
+              code: 'ERR_DATABASE',
+              data: err
+            });
+          });
+      }
+    ],
+    (err, result) => {
+      if (err) return callback(err);
+
+      callback(null, result);
+    }
+  );
+};
+
+exports.transactionDetail = (APP, req, callback) => {
+  let { payment, payment_detail, payment_method, payment_type, transaction_type } = APP.models.mysql;
+  let params = { id: req.body.id };
+
+  async.waterfall(
+    [
+      function checkParams(callback) {
+        if (req.body.id) {
+          callback(null, true);
+        } else {
+          callback({
+            code: 'INVALID_REQUEST',
+            id: '?',
+            message: 'Kesalahan pada parameter id'
+          });
+        }
+      },
+
+      function checkLevel(data, callback) {
+        if (req.user.level === 1 || req.user.level === 2) {
+          callback(null, true);
+        } else {
+          callback({
+            code: 'INVALID_REQUEST',
+            id: '?',
+            message: 'Invalid User Level'
+          });
+        }
+      },
+
+      function getData(data, callback) {
+        // add payment_method to payment
+        payment.belongsTo(payment_method, {
+          targetKey: 'id',
+          foreignKey: 'payment_method_id'
+        });
+
+        // add payment_type to payment_method
+        payment_method.belongsTo(payment_type, {
+          targetKey: 'id',
+          foreignKey: 'payment_type_id'
+        });
+
+        //add payment_detail to payment
+        payment.hasMany(payment_detail, {
+          sourceKey: 'id',
+          foreignKey: 'payment_id'
+        });
+
+        // add transaction_type to payment
+        payment_detail.belongsTo(transaction_type, {
+          targetKey: 'id',
+          foreignKey: 'transaction_type_id'
+        });
+
+        payment
+          .findOne({
+            include: [
+              {
+                model: payment_method,
+                attributes: ['id', 'payment_type_id', 'to_bank_name', 'to_rek_name', 'to_rek_no'],
+                include: [
+                  {
+                    model: payment_type,
+                    attributes: ['id', 'name']
+                  }
+                ]
+              },
+              {
+                model: payment_detail,
+                attributes: ['id', 'payment_id', 'transaction_type_id', 'item_id'],
+                include: [
+                  {
+                    model: transaction_type,
+                    attributes: ['id', 'name']
+                  }
+                ]
+              }
+            ],
+            where: params
+          })
+          .then(rows => {
+            if (rows == null) {
+              callback({
+                code: 'NOT_FOUND',
+                id: '?',
+                message: 'Data tidak ditemukan'
+              });
+            } else {
+              callback(null, {
+                code: 'FOUND',
+                id: '?',
+                message: 'Data ditemukan',
+                data: rows
+              });
+            }
+          })
+          .catch(err => {
+            console.log(err);
+
+            return callback({
+              code: 'ERR_DATABASE',
+              data: err
+            });
+          });
+      }
+    ],
+    (err, result) => {
+      if (err) return callback(err);
+
+      callback(null, result);
+    }
+  );
 };
