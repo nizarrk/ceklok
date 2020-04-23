6b0f450798c8111fef5ccbad8d569fe043eff647
Nizar <niezar.rk@gmail.com>
2020-04-23 10:57:47 +0700

    upload model training facerecog

--- controllers/exampleController.js
+++ controllers/exampleController.js
@@ -147,20 +147,21 @@ exports.testing = async (APP, req, callback) => {
     let fileName = new Date().toISOString().replace(/:|\./g, '');
     let docPath = './public/' + req.body.path;
 
-    // upload file
-    if (req.files.upload) {
-      req.files.upload.mv(docPath + req.files.upload.name, function(err) {
-        if (err)
-          return callback({
-            code: 'ERR'
-          });
-      });
-
+    Promise.all(
+      req.files.upload.map((x, i) => {
+        req.files.upload[i].mv(docPath + req.files.upload[i].name, function(err) {
+          if (err)
+            return callback({
+              code: 'ERR'
+            });
+        });
+      })
+    ).then(() => {
       callback(null, {
         code: 'OK',
         message: 'upload sukses'
       });
-    }
+    });
   } catch (err) {
     console.log(err);
     callback({
