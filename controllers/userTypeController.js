f5e97fe012fd3aae996096afe3d832ea2571691f
Nizar <niezar.rk@gmail.com>
2020-06-11 14:05:17 +0700

    fix bug in edit user type & user type feature

--- controllers/userTypeController.js
+++ controllers/userTypeController.js
@@ -95,6 +95,10 @@ exports.getById = function(APP, req, callback) {
               {
                 model: user_type_feature,
                 attributes: ['id', 'user_type_id', 'subfeature_id'],
+                required: false,
+                where: {
+                  status: 1
+                },
                 include: [
                   {
                     model: subfeature,
@@ -277,58 +281,240 @@ exports.createUserType = function(APP, req, callback) {
 };
 
 exports.updateUserType = function(APP, req, callback) {
-  let { user_type } = APP.models.mysql;
-  user_type
-    .update(
-      {
-        name: req.body.name,
-        description: req.body.desc
+  let query;
+  let user_type_feature;
+  let { name, desc, id, subfeature, status } = req.body;
+
+  async.waterfall(
+    [
+      function checkParam(callback) {
+        if (id) {
+          if (req.user.level === 2) {
+            query = APP.models.company[req.user.db].mysql.user_type;
+            user_type_feature = APP.models.company[req.user.db].mysql.user_type_feature;
+
+            callback(null, true);
+          } else if (req.user.level === 1) {
+            query = APP.models.mysql.user_type;
+            user_type_feature = APP.models.mysql.user_type_feature;
+
+            callback(null, true);
+          } else {
+            callback({
+              code: 'INVALID_REQUEST',
+              message: 'Invalid user level!'
+            });
+          }
+        } else {
+          callback({
+            code: 'INVALID_REQUEST',
+            message: 'Kesalahan pada parameter id!'
+          });
+        }
       },
-      {
-        where: {
-          id: req.body.id
+
+      function checkBody(data, callback) {
+        if (name && desc && id && status) {
+          if (status == '1' || status == '0') {
+            callback(null, true);
+          } else {
+            callback({
+              code: 'INVALID_REQUEST',
+              message: 'Kesalahan pada parameter status!'
+            });
+          }
+        } else {
+          console.log('3');
+
+          callback({
+            code: 'INVALID_REQUEST',
+            message: 'Kesalahan pada parameter!'
+          });
+        }
+      },
+
+      function updateUserType(data, callback) {
+        query
+          .update(
+            {
+              name: name,
+              description: desc,
+              status: status,
+              updated_at: new Date(),
+              action_by: req.user.id
+            },
+            {
+              where: {
+                id: id
+              }
+            }
+          )
+          .then(result => {
+            console.log(result);
+
+            // if (!result || (result && !result[0])) {
+            //   return callback({
+            //     code: 'INVALID_REQUEST',
+            //     message: 'Grade tidak ditemukan!'
+            //   });
+            // }
+
+            return callback(null, true);
+          })
+          .catch(err => {
+            console.log('iki error', err);
+
+            if (err.original && err.original.code === 'ER_EMPTY_QUERY') {
+              let params = 'Error! Empty Query'; //This is only example, Object can also be used
+              return callback({
+                code: 'UPDATE_NONE',
+                data: params
+              });
+            }
+
+            if (err.original && err.original.code === 'ER_DUP_ENTRY') {
+              let params = 'Error! Duplicate Entry'; //This is only example, Object can also be used
+              return callback({
+                code: 'DUPLICATE',
+                data: params
+              });
+            }
+
+            return callback({
+              code: 'ERR_DATABASE',
+              data: err
+            });
+          });
+      },
+
+      function updateUserTypeFeature(data, callback) {
+        if (subfeature === null) {
+          callback(null, {
+            code: 'UPDATE_SUCCESS',
+            message: 'User type berhasil diubah!'
+          });
+        } else {
+          let subfeatures = subfeature.split(','),
+            insert = [],
+            update = [];
+
+          Promise.all(
+            subfeatures.map((x, index) => {
+              return user_type_feature
+                .findOne({
+                  where: {
+                    user_type_id: id,
+                    subfeature_id: x
+                  }
+                })
+                .then(res => {
+                  if (res == null) {
+                    insert.push({
+                      subfeature_id: x,
+                      user_type_id: id
+                    });
+
+                    return user_type_feature
+                      .create({
+                        subfeature_id: x,
+                        user_type_id: id
+                      })
+                      .then(() => {
+                        console.log(`id ${x} inserted`);
+                        return true;
+                      });
+                  }
+                  return res
+                    .update({
+                      status: res.status == 0 ? 1 : 0
+                    })
+                    .then(updated => {
+                      console.log(`id ${x} updated`);
+                      update.push(updated);
+                      return true;
+                    });
+                });
+            })
+          )
+            .then(() => {
+              callback(null, {
+                code: 'UPDATE_SUCCESS',
+                message: 'User type berhasil diubah!',
+                data: {
+                  inserted: insert,
+                  updated: update
+                }
+              });
+            })
+            .catch(err => {
+              console.log(err);
+              callback({
+                code: 'ERR',
+                data: err
+              });
+            });
         }
       }
-    )
-    .then(result => {
-      if (!result || (result && !result[0])) {
-        let params = 'No data updated'; //This is only example, Object can also be used
-        return callback(null, {
-          code: 'UPDATE_NONE',
-          data: params
-        });
+    ],
+    (err, result) => {
+      if (err) {
+        return callback(err);
       }
+      callback(null, result);
+    }
+  );
+  // let { user_type } = APP.models.mysql;
+  // user_type
+  //   .update(
+  //     {
+  //       name: req.body.name,
+  //       description: req.body.desc
+  //     },
+  //     {
+  //       where: {
+  //         id: req.body.id
+  //       }
+  //     }
+  //   )
+  //   .then(result => {
+  //     if (!result || (result && !result[0])) {
+  //       let params = 'No data updated'; //This is only example, Object can also be used
+  //       return callback(null, {
+  //         code: 'UPDATE_NONE',
+  //         data: params
+  //       });
+  //     }
 
-      let params = 'Update Success'; //This is only example, Object can also be used
-      return callback(null, {
-        code: 'UPDATE_SUCCESS',
-        data: params
-      });
-    })
-    .catch(err => {
-      console.log('iki error', err);
+  //     let params = 'Update Success'; //This is only example, Object can also be used
+  //     return callback(null, {
+  //       code: 'UPDATE_SUCCESS',
+  //       data: params
+  //     });
+  //   })
+  //   .catch(err => {
+  //     console.log('iki error', err);
 
-      if (err.original && err.original.code === 'ER_EMPTY_QUERY') {
-        let params = 'Error! Empty Query'; //This is only example, Object can also be used
-        return callback({
-          code: 'UPDATE_NONE',
-          data: params
-        });
-      }
+  //     if (err.original && err.original.code === 'ER_EMPTY_QUERY') {
+  //       let params = 'Error! Empty Query'; //This is only example, Object can also be used
+  //       return callback({
+  //         code: 'UPDATE_NONE',
+  //         data: params
+  //       });
+  //     }
 
-      if (err.original && err.original.code === 'ER_DUP_ENTRY') {
-        let params = 'Error! Duplicate Entry'; //This is only example, Object can also be used
-        return callback({
-          code: 'DUPLICATE',
-          data: params
-        });
-      }
+  //     if (err.original && err.original.code === 'ER_DUP_ENTRY') {
+  //       let params = 'Error! Duplicate Entry'; //This is only example, Object can also be used
+  //       return callback({
+  //         code: 'DUPLICATE',
+  //         data: params
+  //       });
+  //     }
 
-      return callback({
-        code: 'ERR_DATABASE',
-        data: err
-      });
-    });
+  //     return callback({
+  //       code: 'ERR_DATABASE',
+  //       data: err
+  //     });
+  //   });
 };
 
 exports.updateUserTypeFeature = function(APP, req, callback) {
@@ -379,33 +565,117 @@ exports.updateUserTypeFeature = function(APP, req, callback) {
   });
 };
 
-exports.connectFeature = (APP, req, callback) => {};
-
 exports.delete = function(APP, req, callback) {
-  let { user_type } = APP.models.mysql;
-  let params = {
-    where: {
-      id: req.body.id
-    }
-  };
-  user_type
-    .destroy(params)
-    .then(deleted => {
-      if (!deleted)
-        return callback(null, {
-          code: 'DELETE_NONE',
-          data: params.where
-        });
+  let query;
+  let user_type;
+  let user_type_feature;
+  async.waterfall(
+    [
+      function checkParam(callback) {
+        if (req.body.id) {
+          if (req.user.level === 2) {
+            query = APP.models.mysql.admin;
+            user_type = APP.models.company[req.user.db].mysql.user_type;
+            user_type_feature = APP.models.company[req.user.db].mysql.user_type_feature;
 
-      return callback(null, {
-        code: 'DELETE_SUCCESS',
-        data: params.where
-      });
-    })
-    .catch(err => {
-      return callback({
-        code: 'ERR_DATABASE',
-        data: err
-      });
-    });
+            callback(null, true);
+          } else if (req.user.level === 1) {
+            query = APP.models.mysql.admin_app;
+            user_type = APP.models.mysql.user_type;
+            user_type_feature = APP.models.mysql.user_type_feature;
+
+            callback(null, true);
+          } else {
+            callback({
+              code: 'INVALID_REQUEST',
+              message: 'Invalid user level!'
+            });
+          }
+        } else {
+          callback({
+            code: 'INVALID_REQUEST',
+            message: 'Kesalahan pada parameter id!'
+          });
+        }
+      },
+
+      function checkUser(data, callback) {
+        query
+          .findAll({
+            where: {
+              user_type_id: req.body.id
+            }
+          })
+          .then(res => {
+            if (res.length == 0) {
+              callback(null, true);
+            } else {
+              callback({
+                code: 'INVALID_REQUEST',
+                id: '',
+                message: 'Terdapat employee aktif sedang mengunakan grading ini!'
+              });
+            }
+          })
+          .catch(err => {
+            console.log(err);
+            callback({
+              code: 'ERR_DATABASE',
+              id: '?',
+              message: '?',
+              data: err
+            });
+          });
+      },
+
+      function deleteUserType(data, callback) {
+        user_type
+          .destroy({
+            where: {
+              id: req.body.id
+            }
+          })
+          .then(deleted => {
+            if (!deleted)
+              return callback({
+                code: 'INVALID_REQUEST',
+                message: 'Grade tidak ditemukan!'
+              });
+
+            callback(null, deleted);
+          })
+          .catch(err => {
+            return callback({
+              code: 'ERR_DATABASE',
+              data: err
+            });
+          });
+      },
+
+      function deleteGradeBenefit(data, callback) {
+        user_type_feature
+          .destroy({
+            where: {
+              user_type_id: req.body.id
+            }
+          })
+          .then(deleted => {
+            callback(null, {
+              code: 'DELETE_SUCCESS',
+              id: '?',
+              message: '',
+              data: {
+                user_type: data,
+                user_type_feature: deleted
+              }
+            });
+          });
+      }
+    ],
+    (err, result) => {
+      if (err) return callback(err);
+
+      callback(null, result);
+    }
+  );
 };
