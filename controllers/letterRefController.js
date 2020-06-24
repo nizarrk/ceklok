bc3e9dee3648d9e07be3749bdf5f2267454487f5
Nizar <niezar.rk@gmail.com>
2020-06-24 12:41:28 +0700

    change get letter ref into raw query

--- controllers/letterRefController.js
+++ controllers/letterRefController.js
@@ -5,66 +5,67 @@ const trycatch = require('trycatch');
 const path = require('path');
 
 exports.get = (APP, req, callback) => {
-  let { letter_ref, letter, department } = APP.models.company[req.user.db].mysql;
-  let { admin } = APP.models.mysql;
-  let query = {};
+  let query = '';
 
-  if (req.body.status)
-    query = {
-      status: req.body.status
-    };
+  if (req.body.status) query = `WHERE ref.status = ${req.body.status}`;
+
+  if (req.body.department) query = `WHERE dep.id = ${req.body.department}`;
+
+  if (req.body.status && req.body.department)
+    query = `WHERE ref.status = ${req.body.status} AND dep.id = ${req.body.department}`;
 
   if (req.body.datestart && req.body.dateend)
-    query = {
-      created_at: { $between: [req.body.datestart, req.body.dateend] }
-    };
+    query = `WHERE date_format(ref.created_at, '%Y-%m-%d') BETWEEN date_format('${req.body.datestart}', '%Y-%m-%d') AND date_format('${req.body.dateend}', '%Y-%m-%d')`;
 
   if (req.body.datestart && req.body.dateend && req.body.status)
-    query = {
-      status: req.body.status,
-      created_at: { $between: [req.body.datestart, req.body.dateend] }
-    };
+    query = `WHERE ref.status = ${req.body.status} AND date_format(ref.created_at, '%Y-%m-%d') BETWEEN date_format('${req.body.datestart}', '%Y-%m-%d') AND date_format('${req.body.dateend}', '%Y-%m-%d')`;
 
-  letter_ref.belongsTo(letter, {
-    targetKey: 'id',
-    foreignKey: 'letter_id'
-  });
+  if (req.body.datestart && req.body.dateend && req.body.department)
+    query = `WHERE dep.id = ${req.body.department} AND date_format(ref.created_at, '%Y-%m-%d') BETWEEN date_format('${req.body.datestart}', '%Y-%m-%d') AND date_format('${req.body.dateend}', '%Y-%m-%d')`;
 
-  letter.belongsTo(department, {
-    targetKey: 'id',
-    foreignKey: 'department_id'
-  });
-
-  letter_ref.belongsTo(admin, {
-    targetKey: 'id',
-    foreignKey: 'approved_by'
-  });
+  if (req.body.datestart && req.body.dateend && req.body.department && req.body.status)
+    query = `WHERE dep.id = ${req.body.department} AND ref.status = ${req.body.status} AND date_format(ref.created_at, '%Y-%m-%d') BETWEEN date_format('${req.body.datestart}', '%Y-%m-%d') AND date_format('${req.body.dateend}', '%Y-%m-%d')`;
 
   console.log(query);
 
-  letter_ref
-    .findAll({
-      include: [
-        {
-          model: letter,
-          attributes: ['id', 'name', 'description', 'letter_code'],
-          required: false,
-          include: [
-            {
-              model: department,
-              attributes: ['id', 'name', 'description']
-            }
-          ]
-        },
-        {
-          model: admin,
-          attributes: ['id', 'name']
-        }
-      ],
-      where: query
-    })
+  APP.db.sequelize
+    .query(
+      `SELECT 
+          ref.*, 
+          letter.name 'letter_name', 
+          letter.description 'letter_description', 
+          dep.id 'department_id', 
+          dep.name 'department_name', 
+          dep.description 'department_description',
+          adm1.name 'creator_name',
+          adm2.name 'approver_name',
+          adm3.name 'updater_name'
+        FROM
+          ${req.user.db}.letter_ref ref
+        INNER JOIN
+        ${req.user.db}.letter
+        ON
+          ref.letter_id = letter.id
+        INNER JOIN
+        ${req.user.db}.department dep
+        ON
+          letter.department_id = dep.id
+        LEFT JOIN
+          ${process.env.MYSQL_NAME}.admin adm1
+        ON
+          ref.created_by = adm1.id
+        LEFT JOIN
+          ${process.env.MYSQL_NAME}.admin adm2
+        ON
+          ref.approved_by = adm2.id
+        LEFT JOIN
+          ${process.env.MYSQL_NAME}.admin adm3
+        ON
+          ref.updated_by = adm3.id
+        ${query}`
+    )
     .then(res => {
-      if (res.length == 0) {
+      if (res[0].length == 0) {
         callback({
           code: 'NOT_FOUND',
           message: 'Nomor surat tidak ditemukan!'
