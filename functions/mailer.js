ac0beb43ad558207c31e10720dfe91e56a2f462e
Yunizar Isnaini <yvnizar@Yunizars-MacBook-Pro.local>
2020-07-17 15:02:08 +0700

    Fixing SMTP Certificate

--- functions/mailer.js
+++ functions/mailer.js
@@ -17,7 +17,8 @@ exports.sendMail = data => {
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD
-      }
+      },
+      tls: { rejectUnauthorized: false }
     });
 
     let mailOptions = {
