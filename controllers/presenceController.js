b19262c2fa1015bfb1f993d3d15219b1ef37046e
Nizar <niezar.rk@gmail.com>
2020-07-21 15:15:10 +0700

    added get detail presence

--- controllers/presenceController.js
+++ controllers/presenceController.js
@@ -7,9 +7,15 @@ const mkdirp = require('mkdirp');
 
 // fungsinya dipanggil cron
 exports.generateDailyPresence = (APP, req, callback) => {
-  let { presence_setting, presence, presence_monthly, presence_period, employee, schedule } = APP.models.company[
-    `${process.env.MYSQL_NAME}_${req.body.company}`
-  ].mysql;
+  let {
+    presence_setting,
+    presence,
+    presence_monthly,
+    presence_period,
+    employee,
+    schedule,
+    schedule_switch
+  } = APP.models.company[`${process.env.MYSQL_NAME}_${req.body.company}`].mysql;
   async.waterfall(
     [
       function checkSettingCutOffMonthly(callback) {
@@ -280,21 +286,56 @@ exports.generateDailyPresence = (APP, req, callback) => {
                             console.log('cek aja lur!!!', data.user_id);
                           } else {
                             console.log('absen nangdi lur?', data.user_id);
-                            // Data Absent Cuti Not Found == Absent
-                            presence
-                              .update(
-                                {
-                                  presence_setting_id: result.status[4].id, // 'A'
-                                  total_minus: data.schedule.work_time
-                                },
-                                {
-                                  where: {
-                                    id: data.id
-                                  }
+                            // check schdule switch request
+                            schedule_switch
+                              .findAll({
+                                where: {
+                                  status: 1,
+                                  $or: [
+                                    {
+                                      date: yesterday
+                                    },
+                                    {
+                                      target_user_date: yesterday
+                                    }
+                                  ]
                                 }
-                              )
-                              .then(updated => {
-                                console.log('Hasil A', updated);
+                              })
+                              .then(resSwitch => {
+                                if (resSwitch.length == 0) {
+                                  // Data Absent Cuti Switch Not Found == Absent
+                                  presence
+                                    .update(
+                                      {
+                                        presence_setting_id: result.status[4].id, // 'A'
+                                        total_minus: data.schedule.work_time
+                                      },
+                                      {
+                                        where: {
+                                          id: data.id
+                                        }
+                                      }
+                                    )
+                                    .then(updated => {
+                                      console.log('Hasil A', updated);
+                                    });
+                                } else {
+                                  // Data Switch Found == LD
+                                  presence
+                                    .update(
+                                      {
+                                        presence_setting_id: result.status[7].id // 'LD'
+                                      },
+                                      {
+                                        where: {
+                                          id: data.id
+                                        }
+                                      }
+                                    )
+                                    .then(updatedSwitch => {
+                                      console.log('Hasil LD Switch', updatedSwitch);
+                                    });
+                                }
                               });
                           }
                         } else {
@@ -822,9 +863,16 @@ exports.generateDailyPresence = (APP, req, callback) => {
 };
 
 exports.checkInOutProcess = (APP, req, callback) => {
-  let { employee, schedule, device, branch, presence, presence_detail, presence_setting } = APP.models.company[
-    req.user.db
-  ].mysql;
+  let {
+    employee,
+    schedule,
+    schedule_switch,
+    device,
+    branch,
+    presence,
+    presence_detail,
+    presence_setting
+  } = APP.models.company[req.user.db].mysql;
   async.waterfall(
     [
       function checkBLEDevice(callback) {
@@ -847,51 +895,164 @@ exports.checkInOutProcess = (APP, req, callback) => {
           });
       },
 
+      function checkScheduleSwitch(result, callback) {
+        let today = moment().format('YYYY-MM-DD');
+
+        APP.db.sequelize
+          .query(
+            `
+          SELECT 
+              switch.*,
+              req.id AS 'req_id',
+              req.name AS 'req_name',
+              schreq.id AS 'req_schedule_id',
+              schreq.name AS 'req_schedule_name',
+              schreq.work_day AS 'req_schedule_work_day',
+              schreq.work_time AS 'req_schedule_work_time',
+              schreq.total_work_time AS 'req_schedule_total_work_time',
+              schreq.check_in_start AS 'req_schedule_check_in_start',
+              schreq.check_in_end AS 'req_schedule_check_in_end',
+              target.id AS 'target_id',
+              target.name AS 'target_name',
+              schtarget.id AS 'target_schedule_id',
+              schtarget.name AS 'target_schedule_name',
+              schtarget.work_day AS 'target_schedule_work_day',
+              schtarget.work_time AS 'target_schedule_work_time',
+              schtarget.total_work_time AS 'req_schedule_total_work_time',
+              schtarget.check_in_start AS 'target_schedule_check_in_start',
+              schtarget.check_in_end AS 'target_schedule_check_in_end'
+            FROM 
+              ${req.user.db}.schedule_switch switch
+            INNER JOIN
+              ${req.user.db}.employee req
+            ON
+              switch.user_id = req.id
+            INNER JOIN
+              ${req.user.db}.employee target
+            ON
+              switch.target_user_id = target.id
+            INNER JOIN
+              ${req.user.db}.schedule schreq
+            ON
+              req.schedule_id = schreq.id
+            INNER JOIN
+              ${req.user.db}.schedule schtarget
+            ON
+              target.schedule_id = schtarget.id
+            WHERE
+              (switch.user_id = ${req.user.id} OR switch.target_user_id = ${req.user.id})
+            AND
+              (switch.date = '${today}' OR switch.target_user_date = '${today}')
+            ORDER BY 
+              id DESC 
+            LIMIT 1
+            `
+          )
+          .then(res => {
+            if (res[0].length == 0) {
+              callback(null, result);
+            } else {
+              let obj = {};
+
+              // sebagai yang request
+              if (res[0][0].date == today) {
+                obj.id = res[0][0].req_schedule_id;
+                obj.work_day = res[0][0].req_schedule_work_day.split(',');
+                obj.name = res[0][0].req_schedule_name;
+                obj.work_time = res[0][0].req_schedule_work_time;
+                obj.check_in_start = res[0][0].req_schedule_check_in_start;
+                obj.check_in_end = res[0][0].req_schedule_check_in_end;
+                obj.check_out_start = res[0][0].req_schedule_check_out_start;
+                obj.check_out_end = res[0][0].req_schedule_check_out_end;
+              }
+
+              // sebagai yang dituju
+              if (res[0][0].target_user_date == today) {
+                obj.id = res[0][0].target_schedule_id;
+                obj.work_day = res[0][0].target_schedule_work_day.split(',');
+                obj.name = res[0][0].target_schedule_name;
+                obj.work_time = res[0][0].target_schedule_work_time;
+                obj.check_in_start = res[0][0].target_schedule_check_in_start;
+                obj.check_in_end = res[0][0].target_schedule_check_in_end;
+                obj.check_out_start = res[0][0].target_schedule_check_out_start;
+                obj.check_out_end = res[0][0].target_schedule_check_out_end;
+              }
+
+              callback(null, {
+                device: result,
+                schedule: obj
+              });
+            }
+          })
+          .catch(err => {
+            console.log(err, 'Error checkScheduleSwitch');
+            callback({
+              code: 'ERR_DATABASE',
+              message: 'Error checkScheduleSwitch',
+              data: err
+            });
+          });
+      },
+
       function checkSchedule(result, callback) {
+        if (result.schedule) return callback(null, result);
+
+        employee.belongsTo(schedule, {
+          targetKey: 'id',
+          foreignKey: 'schedule_id'
+        });
+
         employee
           .findOne({
+            attributes: ['id', 'schedule_id', 'nik', 'name'],
+            include: [
+              {
+                model: schedule,
+                required: true
+              }
+            ],
             where: {
               id: req.user.id
             }
           })
-          .then(user => {
-            schedule
-              .findOne({
-                where: {
-                  id: user.schedule_id
-                }
-              })
-              .then(res => {
-                if (res === null) {
-                  callback({
-                    code: 'NOT_FOUND',
-                    message: 'Schedule tidak ditemukan.'
-                  });
-                } else {
-                  // cek hari ini apakah hari kerja
-                  let workDay = res.work_day;
-
-                  workDay = workDay.filter(x => {
-                    return x == moment().day();
-                  });
-
-                  if (workDay.length == 0) {
-                    console.log('libur');
-
-                    callback({
-                      code: 'INVALID_REQUEST',
-                      message: 'Hari ini bukan hari kerja!'
-                    });
-                  } else {
-                    console.log('kerja');
-
-                    callback(null, {
-                      device: result,
-                      schedule: res
-                    });
-                  }
-                }
+          .then(res => {
+            if (res === null) {
+              callback({
+                code: 'NOT_FOUND',
+                message: 'Schedule tidak ditemukan.'
               });
+            } else {
+              // cek hari ini apakah hari kerja
+              let workDay = res.schedule.work_day;
+
+              workDay = workDay.filter(x => {
+                return x == moment().day();
+              });
+
+              if (workDay.length == 0) {
+                console.log('libur');
+
+                callback({
+                  code: 'INVALID_REQUEST',
+                  message: 'Hari ini bukan hari kerja!'
+                });
+              } else {
+                console.log('kerja');
+
+                callback(null, {
+                  device: result,
+                  schedule: res.schedule
+                });
+              }
+            }
+          })
+          .catch(err => {
+            console.log(err, 'Error checkSchedule');
+            callback({
+              code: 'ERR_DATABASE',
+              message: 'Error checkSchedule',
+              data: err
+            });
           });
       },
 
@@ -1321,6 +1482,87 @@ exports.getHistoryCheckInOut = (APP, req, callback) => {
         info: {
           dataCount: rows[0].length
         }
+      });
+    })
+    .catch(err => {
+      console.log(err);
+
+      return callback({
+        code: 'ERR_DATABASE',
+        data: err
+      });
+    });
+};
+
+exports.getDetailCheckInOut = (APP, req, callback) => {
+  if (!req.body.id) {
+    return callback({
+      code: 'INVALID_REQUEST',
+      message: 'Kesalahan pada parameter id!'
+    });
+  }
+
+  let params = ` WHERE presence.id = ${req.body.id}`;
+
+  if (req.user.level === 3) {
+    params = ` WHERE presence.user_id = ${req.user.id} AND presence.id = ${req.body.id}`;
+  }
+
+  let query = `SELECT 
+                presence.id, presence.user_id, presence.check_in_device_id, 
+                presence.check_out_device_id, presence.check_in_branch_id, 
+                presence.check_out_branch_id, presence.date, presence.check_in, 
+                presence.check_out, presence.total_time, presence.presence_setting_id, 
+                detail.latitude_checkin AS 'check_in_latitude',
+                detail.longitude_checkin AS 'check_in_longitude',
+                detail.latitude_checkout AS 'check_out_latitude',
+                detail.longitude_checkout AS 'check_out_longitude',
+                detail.image_checkin_a AS 'check_in_image_a',
+                detail.image_checkin_b AS 'check_in_image_b',
+                detail.image_checkin_c AS 'check_in_image_c',
+                detail.image_checkout_a AS 'check_out_image_a',
+                detail.image_checkout_b AS 'check_out_image_b',
+                detail.image_checkout_c AS 'check_out_image_c',
+                employee.id AS 'employee_id', 
+                employee.nik AS 'employee_nik', 
+                employee.name AS 'employee_name', 
+                employee.photo AS 'employee_photo', 			
+                check_in_device.name AS 'check_in_device_name',
+                check_in_device.location AS 'check_in_device_location',
+                check_in_device.major AS 'check_in_device_major',
+                check_in_device.minor AS 'check_in_device_minor',
+                check_out_device.name AS 'check_out_device_name',
+                check_out_device.location AS 'check_out_device_location',
+                check_in_branch.name AS 'check_in_branch_name',
+                check_in_branch.address AS 'check_in_branch_address',
+                check_out_branch.name AS 'check_out_branch_name',
+                check_out_branch.address AS 'check_out_branch_address',
+                presence_setting.value AS 'presence_status_name',
+                presence_setting.description AS 'presence_status_description'
+              FROM 
+                ${req.user.db}.presence AS presence 
+              LEFT OUTER JOIN
+              ${req.user.db}.presence_detail AS detail ON presence.id = detail.presence_id
+              LEFT OUTER JOIN 
+                ${req.user.db}.employee AS employee ON presence.user_id = employee.id 
+              LEFT OUTER JOIN 
+                ${req.user.db}.device AS check_in_device ON presence.check_in_device_id = check_in_device.id 
+              LEFT OUTER JOIN 
+                ${req.user.db}.device AS check_out_device ON presence.check_out_device_id = check_out_device.id 
+              LEFT OUTER JOIN 
+                ${req.user.db}.branch AS check_in_branch ON presence.check_in_branch_id = check_in_branch.id 
+              LEFT OUTER JOIN 
+                ${req.user.db}.branch AS check_out_branch ON presence.check_out_branch_id = check_out_branch.id
+              LEFT OUTER JOIN
+                ${req.user.db}.presence_setting AS presence_setting ON presence.presence_setting_id = presence_setting.id  
+              ${params}`;
+
+  APP.db.sequelize
+    .query(query)
+    .then(rows => {
+      return callback(null, {
+        code: rows[0] && rows[0].length > 0 ? 'FOUND' : 'NOT_FOUND',
+        data: rows[0][0]
       });
     })
     .catch(err => {
