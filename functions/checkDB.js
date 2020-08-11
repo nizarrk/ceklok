b71194bd2e9eed415fc4dff7b29bdbf1060cdce6
Nizar <niezar.rk@gmail.com>
2020-08-11 12:44:40 +0700

    added function to check db avbaibility

---
+++ functions/checkDB.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const db = require('../db');
+
+module.exports = input => {
+  return db.sequelize
+    .query(`SHOW DATABASES LIKE '${process.env.MYSQL_NAME}_${input}'`)
+    .then(res => {
+      return res[0];
+    })
+    .catch(err => {
+      console.log(err);
+      return err;
+    });
+};
