e40e2f5ddb077f693aa2f281cd4eac9ed47ed977
Nizar <niezar.rk@gmail.com>
2020-08-18 13:50:53 +0700

    added income deduction

---
+++ models/template/income_deduction.js
@@ -0,0 +1,69 @@
+'use strict';
+
+module.exports = function(sequelize, Sequelize) {
+  let Model = sequelize.define(
+    'income_deduction',
+    {
+      id: {
+        type: Sequelize.INTEGER(11),
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+        unique: true
+      },
+      code: {
+        type: Sequelize.STRING(255)
+      },
+      name: {
+        type: Sequelize.STRING(255)
+      },
+      description: {
+        type: Sequelize.STRING(255)
+      },
+      percentage: {
+        type: Sequelize.INTEGER
+      },
+      nominal: {
+        type: Sequelize.INTEGER
+      },
+      type: {
+        type: Sequelize.INTEGER // 1 = income, 2 = deduction
+      },
+      status: {
+        type: Sequelize.INTEGER(1), //0 = non aktif, 1 = aktif
+        allowNull: false,
+        defaultValue: 1
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW
+      },
+      created_by: {
+        type: Sequelize.INTEGER(10),
+        defaultValue: 0,
+        allowNull: false
+      },
+      updated_by: {
+        type: Sequelize.INTEGER(10),
+        defaultValue: 0,
+        allowNull: false
+      },
+      ip_address: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+        defaultValue: '127.0.0.1'
+      }
+    },
+    {}
+  );
+
+  // Model.associate = function(models) {};
+
+  return Model;
+};
